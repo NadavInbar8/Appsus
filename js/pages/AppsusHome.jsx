e7542f13779bf64b4d87e4dc07e1c74779c3be1e
NadavInbar8 <nadavfrc@gmail.com>
2021-12-29 10:04:26 +0200

    added home placeholder and changed the index title

---
+++ js/pages/AppsusHome.jsx
@@ -0,0 +1,13 @@
+export function AppsusHome() {
+  return (
+    <div className='appsus-home'>
+      <h1>Wellcome to Appsus</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        corrupti, aliquam libero suscipit beatae officia expedita? Fugiat
+        nostrum pariatur fugit veritatis cumque? Earum esse ipsum aut
+        laudantium, incidunt corrupti quisquam.
+      </p>
+    </div>
+  );
+}
