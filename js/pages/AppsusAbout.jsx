d7bf48c94464bff5fc242b44d9dc2c27d09a8097
NadavInbar8 <nadavfrc@gmail.com>
2021-12-29 10:13:13 +0200

    added assets and about page

---
+++ js/pages/AppsusAbout.jsx
@@ -0,0 +1,23 @@
+export function AppsusAbout() {
+  return (
+    <div className='about'>
+      <h1>About us</h1>
+      <img className='about-pic' src='./assets/imgs/Nadav.png' />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus nobis
+        fugiat amet quaerat maxime deserunt laudantium nulla reprehenderit, odio
+        alias adipisci doloribus voluptate rem. Animi magnam alias aut excepturi
+        ullam.
+      </p>
+      <img className='about-pic' src='./assets/imgs/oded.png' />
+      <div>
+        <p>
+          my name iss oded kovo im 23 years old and live in Israel,ness ziona. i
+          served in the IDF as a fighter in the k9 israeli unit named Oketz. now
+          im in coding academy bootcamp learning full stack development and in
+          this site you can see some of my projects and work.
+        </p>
+      </div>
+    </div>
+  );
+}
