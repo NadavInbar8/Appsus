189dc57fad1ff0081189e2b11b213b2ed1a09f89
NadavInbar8 <nadavfrc@gmail.com>
2021-12-31 10:05:04 +0200

    finished minimun requirements for the sprint and started working on bonus features

--- apps/mail/componets/MailPreview.jsx
+++ apps/mail/componets/MailPreview.jsx
@@ -1,38 +1,60 @@
 const { Link } = ReactRouterDOM;
 
-export function MailPreview({ mail, togglePreview, onMoveToTrash }) {
+export function MailPreview({
+  mail,
+  togglePreview,
+  onMoveToTrash,
+  toggleStar,
+  toggleRead,
+}) {
   return (
-    <Link className='clean-link' to={'/mail'}>
-      <div>
-        {!mail.isOpen ? (
-          <div
-            className={
-              mail.isRead ? 'mail-preview flex' : 'mail-preview bold flex'
-            }
-            onClick={togglePreview}
-          >
-            <div className='star-preview'>⭐</div>
+    <div>
+      {!mail.isOpen ? (
+        <div
+          className={
+            mail.isRead ? 'mail-preview flex' : 'mail-preview bold flex'
+          }
+        >
+          <div className='star-preview' onClick={toggleStar}>
+            {!mail.star ? (
+              <img src='../../../assets/SVG/notFilledStar.svg' alt='star' />
+            ) : (
+              <img src='../../../assets/SVG/filledStar.svg' alt='star' />
+            )}
+          </div>
+          <div className='clickable flex' onClick={togglePreview}>
             <div className='from-preview'>{mail.from}</div>
             <div className='subject-preview'>{mail.subject}</div>
             <div className='text-preview'>{mail.body}</div>
+          </div>
+          <div className='preview-options'>
+            <Link className='clean-link' to={`/mail/${mail.id}`}>
+              Expand
+            </Link>
+            <button onClick={toggleRead}>
+              {mail.isRead ? 'mark as unread' : 'mark as read'}
+            </button>
             <button className='delete' onClick={onMoveToTrash}>
               move to trash
             </button>
           </div>
-        ) : (
-          <div
-            className='mail-preview-open flex-column'
-            onClick={togglePreview}
-          >
-            <h1 className='subject-preview-open'>{mail.subject}</h1>
-            <div className='from-preview-open flex'>
-              <h2>{mail.from}</h2> <h3>{mail.to}</h3>
-            </div>
-            <div className='text-preview-open'>{mail.body}</div>
-            <div className='star-preview-open'>⭐</div>
+        </div>
+      ) : (
+        <div className='mail-preview-open flex-column' onClick={togglePreview}>
+          <h1 className='subject-preview-open'>{mail.subject}</h1>
+          <div className='from-preview-open flex'>
+            <h2>{mail.from}</h2> <h3>{mail.to}</h3>
           </div>
-        )}
-      </div>
-    </Link>
+          <div className='text-preview-open'>{mail.body}</div>
+          <div className='star-preview' onClick={toggleStar}>
+            {!mail.star ? (
+              <img src='../../../assets/SVG/notFilledStar.svg' alt='star' />
+            ) : (
+              <img src='../../../assets/SVG/filledStar.svg' alt='star' />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,8 +1,45 @@
+import { mailService } from '../services/mail.service.js';
+import { MailHeader } from '../componets/MailHeader.jsx';
+import { MailFolderList } from '../componets/MailFolderList.jsx';
+
 export class MailDetails extends React.Component {
   state = {
-    mail: null,
+    mail: '',
   };
+
+  componentDidMount() {
+    this.loadMail();
+  }
+
+  loadMail = () => {
+    const { mailId } = this.props.match.params;
+    mailService.getMailById(mailId).then((mail) => {
+      if (!mail) return this.props.history.push('/mail');
+      this.setState({ mail });
+    });
+  };
+  onGoBack = () => {
+    this.props.history.push('/mail');
+  };
+
   render() {
-    return <div>hello world</div>;
+    const { mail } = this.state;
+    if (!mail) return <div>loading</div>;
+    return (
+      <div className='mail-details flex-colunm'>
+        <h1>{mail.subject}</h1>
+        <div className='details-from-to'>
+          <h2>{mail.from}</h2>
+          <span>{mail.fromEmail}</span>
+          <h5>to me</h5>
+        </div>
+        <div className='details-body'>
+          <p>{mail.body}</p>
+        </div>
+        <button className='primary-btn' onClick={this.onGoBack}>
+          Go back
+        </button>
+      </div>
+    );
   }
 }
